5d7f253db416f18a2b65bb5f3acbd1ee20bd4365
mahmoud <mahmoud.kheyrati.fard@gmail.com>
2023-06-09 14:14:08 +0330

    fix(server): add scylla client

--- server/pkg/scylla/scylla.go
+++ server/pkg/scylla/scylla.go
@@ -1,1 +1,20 @@
 package scylla
+
+import (
+	"fmt"
+	"github.com/gocql/gocql"
+	"github.com/scylladb/gocqlx/v2"
+)
+
+func NewScyllaSession(host string, port int, defaultKeyspace, username, password string) (gocqlx.Session, error) {
+	cluster := gocql.NewCluster(fmt.Sprintf("%s:%d", host, port))
+	cluster.Keyspace = defaultKeyspace
+	cluster.AuthProvider = func(h *gocql.HostInfo) (gocql.Authenticator, error) {
+		return gocql.PasswordAuthenticator{
+			Username: username,
+			Password: password,
+		}, nil
+	}
+	session, err := gocqlx.WrapSession(cluster.CreateSession())
+	return session, err
+}
