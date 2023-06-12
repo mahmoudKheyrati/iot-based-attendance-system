55c6d6988da76eb110b742f24fe694fac9fdd7d4
mahmoud <mahmoud.kheyrati.fard@gmail.com>
2023-06-12 09:32:17 +0330

    fix(scylla): add device_id and certs to database schema

--- deployments/database/init_database.sql
+++ deployments/database/init_database.sql
@@ -1,5 +1,17 @@
 create keyspace if not exists attendance_system with replication = {'class': 'NetworkTopologyStrategy'};
 
+create type if not exists Point(lat double, long double);
+
+create table if not exists attendance_system.device
+(
+    id       uuid,
+    name     text,
+    location Point,
+    cert     text,
+    rules    set<text>,
+    primary key ( id),
+);
+
 create table if not exists attendance_system.admin
 (
     username        uuid,
@@ -11,29 +23,39 @@ create table if not exists attendance_system.admin
     primary key ( username, password_hash)
 );
 
+create index if not exists on attendance_system.admin (username) with options = {'unique': 'true'};
 create table if not exists attendance_system.employee
 (
-    id         uuid,
-    first_name text,
-    last_name  text,
-    caKeyHash  text,
-    rules      set<text>,
+    id           uuid,
+    first_name   text,
+    last_name    text,
+    email        text,
+    phone_number text,
+    caKeyHash    text,
+    rules        set<text>,
     primary key ( id )
 );
 
+create index if not exists on attendance_system.employee (id) with options = {'unique': 'true'};
+
 create table if not exists attendance_system.attendance_log
 (
-    timestamp timestamp,
-    user_id   uuid,
-    action    text,
+    timestamp     timestamp,
+    employee_id   uuid,
+    action        text,
+    device_id     text,
+    device_cert   text,
+    employee_cert text,
     seen_by_admin boolean,
-    primary key ( timestamp, user_id, action )
+    primary key ( timestamp, employee_id, action )
 );
+
 
 
 create table attendance_system.admin_commands_log
 (
     timestamp timestamp,
+    device_id text,
     command   text,
     processed boolean,
     primary key (timestamp, command, processed)
