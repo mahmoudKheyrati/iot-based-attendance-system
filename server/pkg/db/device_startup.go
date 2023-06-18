da4be8828e5df7c18c60eb4b17708864b87abb2c
mahmoud <mahmoud.kheyrati.fard@gmail.com>
2023-06-18 19:47:26 +0330

    fix(server/scylla-query): add device_startup repo

---
+++ server/pkg/db/device_startup.go
@@ -0,0 +1,55 @@
+package db
+
+import (
+	"github.com/gocql/gocql"
+	"time"
+)
+
+type DeviceStartup struct {
+	DeviceID          gocql.UUID
+	ServerTimestamp   time.Time
+	TimeAfterStartSec int
+}
+
+type DeviceStartupRepo struct {
+	session *gocql.Session
+}
+
+func NewDeviceStartupRepo(session *gocql.Session) *DeviceStartupRepo {
+	return &DeviceStartupRepo{session: session}
+}
+
+type DeviceLastStartup struct {
+	DeviceID    gocql.UUID
+	LastStartup time.Time
+}
+
+func (d *DeviceStartupRepo) InsertDeviceStartup(deviceStartup DeviceStartup) error {
+
+	query := "INSERT INTO attendance_system.device_startup (device_id, server_timestamp, time_after_start_sec) VALUES (?, ?, ?)"
+
+	err := d.session.Query(query, deviceStartup.DeviceID, deviceStartup.ServerTimestamp, deviceStartup.TimeAfterStartSec).Exec()
+	return err
+}
+
+func (d *DeviceStartupRepo) GetDeviceLastStartupById(dId string) (time.Time, int, error) {
+
+	deviceId, err := gocql.UUIDFromBytes([]byte(dId))
+	if err != nil {
+		return time.Time{}, 0, err
+	}
+
+	query := "SELECT server_timestamp, time_after_start_sec FROM attendance_system.device_startup WHERE device_id = ? ORDER BY server_timestamp DESC LIMIT 1"
+
+	iter := d.session.Query(query, deviceId).Iter()
+
+	var lastStartup time.Time
+	var timeAfterStartSec int
+
+	if iter.Scan(&lastStartup, &timeAfterStartSec) {
+		lastStartup = lastStartup.Add(time.Duration(timeAfterStartSec) * time.Second)
+	}
+
+	err = iter.Close()
+	return lastStartup, timeAfterStartSec, err
+}
