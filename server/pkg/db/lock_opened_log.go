fd1b56263640fc023b4f90432c951b0e8d0b4c2d
mahmoud <mahmoud.kheyrati.fard@gmail.com>
2023-06-18 20:09:19 +0330

    fix(server/scylla-query): add lock_opened_log repo

---
+++ server/pkg/db/lock_opened_log.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	"github.com/gocql/gocql"
+	"time"
+)
+
+type LockOpenedLog struct {
+	DeviceID            string
+	ServerTimestamp     time.Time
+	TimeAfterStartupSec int
+}
+
+type LockOpenedLogRepo struct {
+	session *gocql.Session
+}
+
+func (l *LockOpenedLogRepo) Insert(lockOpenedLog LockOpenedLog) error {
+	// Construct the INSERT query
+	query := "INSERT INTO attendance_system.lock_opened_log (device_id, server_timestamp, time_after_startup_sec) VALUES (?, ?, ?)"
+
+	// Execute the query with the given lock opened log data
+	err := l.session.Query(query, lockOpenedLog.DeviceID, lockOpenedLog.ServerTimestamp, lockOpenedLog.TimeAfterStartupSec).Exec()
+	return err
+}
