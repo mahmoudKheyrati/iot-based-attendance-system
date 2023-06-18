defd95daaba20c3b071097b9700fb168d7f8427f
mahmoud <mahmoud.kheyrati.fard@gmail.com>
2023-06-18 20:11:04 +0330

    fix(server/scylla-query): add device_state_log repo

---
+++ server/pkg/db/device_state_log.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"github.com/gocql/gocql"
+	"time"
+)
+
+type DeviceStateLog struct {
+	DeviceID            string
+	ServerTimestamp     time.Time
+	TimeAfterStartupSec int
+	LedRed              bool
+	LedGreen            bool
+	LedBlue             bool
+}
+
+type DeviceStateLogRepo struct {
+	session *gocql.Session
+}
+
+func (d *DeviceStateLogRepo) Insert(deviceStateLog DeviceStateLog) error {
+	query := "INSERT INTO attendance_system.device_state_log (device_id, server_timestamp, time_after_startup_sec, led_red, led_green, led_blue) VALUES (?, ?, ?, ?, ?, ?)"
+
+	// Execute the query with the given device state log data
+	err := d.session.Query(query, deviceStateLog.DeviceID, deviceStateLog.ServerTimestamp, deviceStateLog.TimeAfterStartupSec, deviceStateLog.LedRed, deviceStateLog.LedGreen, deviceStateLog.LedBlue).Exec()
+	return err
+}
