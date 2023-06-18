49ea8f2560bdc8674b9003ede0c2f445438982d4
mahmoud <mahmoud.kheyrati.fard@gmail.com>
2023-06-18 21:44:15 +0330

    fix(server/apis): implement openDoor rpc

--- server/api/api.go
+++ server/api/api.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"server/config"
@@ -37,13 +38,24 @@ func (a *AttendanceSystem) LockOpenedHistory(request *attendance_system.LockOpen
 func (a *AttendanceSystem) ChangeLedColor(ctx context.Context, request *attendance_system.ChangeLedColorRequest) (*attendance_system.ChangeLedColorResponse, error) {
 	payloadLedChangeColor := fmt.Sprintf("%d,LED_CHANGE_COLOR_%d%d%d", time.Now().Unix(), request.Red, request.Green, request.Blue)
 
-	a.mqttPublisherClient.Publish(fmt.Sprintf("%s/%s", a.config.TopicNames.AdminCommand, request.DeviceId), mqtt2.ExactlyOnce, false, payloadLedChangeColor)
+	token := a.mqttPublisherClient.Publish(fmt.Sprintf("%s/%s", a.config.TopicNames.AdminCommand, request.DeviceId), mqtt2.ExactlyOnce, false, payloadLedChangeColor)
+	token.Wait()
+	if err := token.Error(); err != nil {
+		return nil, errors.New("led color not changed")
+	}
+
 	return &attendance_system.ChangeLedColorResponse{}, nil
 }
 
 func (a *AttendanceSystem) OpenDoor(ctx context.Context, request *attendance_system.OpenDoorRequest) (*attendance_system.OpenDoorResponse, error) {
-	//TODO implement me
-	panic("implement me")
+	payloadOpenDoor := fmt.Sprintf("%d,%s", time.Now().Unix(), "LOCK_OPEN_PERMITTED")
+	token := a.mqttPublisherClient.Publish(fmt.Sprintf("%s/%s", a.config.TopicNames.AdminCommand, request.DeviceId), mqtt2.ExactlyOnce, false, payloadOpenDoor)
+	token.Wait()
+	if err := token.Error(); err != nil {
+		return nil, errors.New("door not opened")
+	}
+	return &attendance_system.OpenDoorResponse{}, nil
+
 }
 
 func (a *AttendanceSystem) GetAllDevices(ctx context.Context, request *attendance_system.GetDeviceIdsRequest) (*attendance_system.GetDeviceIdsResponse, error) {
